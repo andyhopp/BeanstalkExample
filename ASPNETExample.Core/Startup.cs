7d5018cb0cb4483f5a8b678eb0693dcec7c76458
Andy Hopper <andyhopp@amazon.com>
2020-06-29 18:00:22 -0400

    added xray

--- ASPNETExample.Core/Startup.cs
+++ ASPNETExample.Core/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +13,17 @@ namespace BeanstalkExample
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +33,9 @@ namespace BeanstalkExample
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseExceptionHandler("/Error");
+            app.UseXRay("ASPNETExample");
 
             app.UseStaticFiles();
             app.UseRouting();
