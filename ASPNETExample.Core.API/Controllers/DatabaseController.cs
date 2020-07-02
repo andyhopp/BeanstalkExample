1f9cd6d59db8c328412765ef7cc6297b8d514342
Andy Hopper <andyhopp@amazon.com>
2020-07-02 09:37:25 -0400

    Moved database resolution to startup.

--- ASPNETExample.Core.API/Controllers/DatabaseController.cs
+++ ASPNETExample.Core.API/Controllers/DatabaseController.cs
@@ -10,45 +10,26 @@ namespace ASPNETExample.Core.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class DatabaseController : ControllerBase
+    public partial class DatabaseController : ControllerBase
     {
         private readonly ILogger<DatabaseController> _logger;
 
-        public DatabaseController(ILogger<DatabaseController> logger)
+        public DatabaseConnectionInfo ConnectionInfo { get; }
+
+        public DatabaseController(ILogger<DatabaseController> logger, DatabaseConnectionInfo connectionInfo)
         {
             _logger = logger;
-        }
-
-        class DatabaseConnectionInfo
-        {
-            public string Username { get; set; }
-            public string Password { get; set; }
-            public string Engine { get; set; }
-            public string Host { get; set; }
+            ConnectionInfo = connectionInfo;
         }
 
         [HttpGet]
         public async Task<IEnumerable<string>> Get()
         {
-            var tags = await new Amazon.EC2.AmazonEC2Client().DescribeTagsAsync();
-            var passwordArnTag = tags.Tags.FirstOrDefault(T => T.Key == "DBSecretArn");
-            var password = await new Amazon.SecretsManager.AmazonSecretsManagerClient().GetSecretValueAsync(new Amazon.SecretsManager.Model.GetSecretValueRequest
-            {
-                SecretId = passwordArnTag.Value
-            });
-
-            var secretString = password.SecretString;
-            var dbInfo = System.Text.Json.JsonSerializer.Deserialize<DatabaseConnectionInfo>(
-                secretString, 
-                new JsonSerializerOptions {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
-
             var databases = new List<string>();
             var connectionStringBuilder = new System.Data.SqlClient.SqlConnectionStringBuilder();
-            connectionStringBuilder.DataSource = dbInfo.Host;
-            connectionStringBuilder.UserID = dbInfo.Username;
-            connectionStringBuilder.Password = dbInfo.Password;
+            connectionStringBuilder.DataSource = ConnectionInfo.Host;
+            connectionStringBuilder.UserID = ConnectionInfo.Username;
+            connectionStringBuilder.Password = ConnectionInfo.Password;
             connectionStringBuilder.InitialCatalog = "master";
             using (var sqlConnection = new System.Data.SqlClient.SqlConnection(connectionStringBuilder.ConnectionString))
             using (var sqlCommand = new Amazon.XRay.Recorder.Handlers.SqlServer.TraceableSqlCommand("SELECT name from sys.databases", sqlConnection, true))
--- ASPNETExample.Core.API/Startup.cs
+++ ASPNETExample.Core.API/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,8 +26,35 @@ namespace ASPNETExample.Core.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            RegisterDatabaseInfo(services).Wait();
             services.AddControllers();
             services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+        }
+
+        private async Task RegisterDatabaseInfo(IServiceCollection services)
+        {
+            var tags = await new Amazon.EC2.AmazonEC2Client().DescribeTagsAsync();
+            var passwordArnTag = tags.Tags.FirstOrDefault(T => T.Key == "DBSecretArn");
+#if DEBUG
+            passwordArnTag = new Amazon.EC2.Model.TagDescription
+            {
+                Value = "arn:aws:secretsmanager:us-east-1:982831078337:secret:qapi/prod/databaseCredentials-wJWBeO"
+            };
+#endif
+            var password = await new Amazon.SecretsManager.AmazonSecretsManagerClient().GetSecretValueAsync(new Amazon.SecretsManager.Model.GetSecretValueRequest
+            {
+                SecretId = passwordArnTag.Value
+            });
+
+            var secretString = password.SecretString;
+            var dbInfo = System.Text.Json.JsonSerializer.Deserialize<DatabaseConnectionInfo>(
+                secretString,
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+            services.AddSingleton(dbInfo);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
