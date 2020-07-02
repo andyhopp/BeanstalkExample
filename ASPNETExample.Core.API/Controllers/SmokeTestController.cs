26c71b389a48db8d8fdfeaef42eb27d12514c818
Andy Hopper <andyhopp@amazon.com>
2020-07-02 11:32:51 -0400

    Added smoke test.

---
+++ ASPNETExample.Core.API/Controllers/SmokeTestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNETExample.Core.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SmokeTestController : ControllerBase
+    {
+        private readonly ILogger<SmokeTestController> _logger;
+
+        public SmokeTestController(ILogger<SmokeTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList.Where(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).First().ToString();
+        }
+    }
+}
---
+++ ASPNETExample.Core.API/DatabaseConnectionInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPNETExample.Core.API
+{
+    public class DatabaseConnectionInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Engine { get; set; }
+        public string Host { get; set; }
+    }
+}
